84e820a4ed3bb3cc1e0e22191440e526e63a3469
lisafaggiolo <lisa.faggiolo@live.ca>
2020-08-27 01:01:04 +0000

    refactoring of constants

--- client.js
+++ client.js
@@ -1,18 +1,19 @@
 const net = require('net');
 const process = require('process')
+const { IP, PORT} = require('./constants')
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
+    host: IP,
+    port: PORT
   });
  
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
-  conn.on('connect', () => {console.log("Successfully connected to game server")});
-  conn.on('data', (data) => {console.log(data)});
+  conn.on('connect', ()    => {console.log("Successfully connected to game server")});
+  conn.on('data', (data)   => {console.log(data)});
   conn.write("Name: LFF"); 
   //conn.write('Move: up');
   //conn.write('Move: right');
